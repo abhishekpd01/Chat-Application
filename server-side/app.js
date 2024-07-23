2c8a880abfb31572d07999fcc8bb99ce0f16ed5e
Abhishek Prasad <prasadabhishek550@gmail.com>
2024-07-23 17:27:52 +0530

    set up boiler code for server side

---
+++ server-side/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3000, ()=> {
+    console.log("App is up and running on port 3000.");
+});
