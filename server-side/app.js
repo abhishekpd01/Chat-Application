70c444dfb29fd9e82a8241f8898898e79f9e303f
Abhishek Prasad <prasadabhishek550@gmail.com>
2024-07-23 17:52:37 +0530

    add user authentication using chatengine put request

--- server-side/app.js
+++ server-side/app.js
@@ -1,5 +1,9 @@
+const config = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
+
+const privateKey = process.env.Private_key;
 
 const app = express();
 app.use(express.json());
@@ -7,6 +11,18 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+  try {
+    const response = await axios.put(
+        `https://api.chatengine.io/users/`,
+        {username: username, secret: username, first_name: username},
+        {headers: {'private-key': `2d56c779-51e0-4244-b8e0-1c0c8e977106`}}
+    )
+
+    return res.status(response.status).json(response.data);
+} catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+    
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
