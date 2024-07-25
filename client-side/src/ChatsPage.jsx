c77d4048440dca6be12d16917cb49c43eb00d7f2
Abhishek Prasad <prasadabhishek550@gmail.com>
2024-07-25 21:39:26 +0530

    Added ui for ChatsPage

--- client-side/src/ChatsPage.jsx
+++ client-side/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{height: '100vh'}}>
+            <PrettyChatWindow
+                projectId="7ade1674-ae07-4330-8fb7-3daaba8cf007"
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100vh' }}
+            />
+        </div>
+    );
 }
 
 export default ChatsPage
